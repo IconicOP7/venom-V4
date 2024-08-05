fbaf238bb9f2da563d2ccac713cfbaa6a946de9e
IconicOP7 <utkarshkumar4242@gmail.com>
2024-08-05 19:58:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7228280948:AAHUXvfEr8DOUJBdzvxgzoGNEUonxh_0C8E")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("EXR_ICONIC")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1197720408")
 
 
 
